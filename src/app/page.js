413c0d0adefde9c261cb305510e92468e63716aa
probeshnath <probeshn@gmail.com>
2023-12-12 19:29:20 +0600

    pic and hero section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,14 @@
-import Link from "next/link"
+import Header from "@/components/layout/Header"
+import Hero from "@/components/layout/Hero"
+
 
 
 const Home = () => {
   return (
     <>
-    <header className="flex items-center justify-between ">
-      <Link className="text-primary font-semibold text-2xl" href="">PIZZA GHOR</Link>
-      <nav className="flex gap-4">
-      <Link href={'/'}>Home</Link>
-          <Link href={'/menu'}>Menu</Link>
-          <Link href={'/#about'}>About</Link>
-          <Link href={'/#contact'}>Contact</Link>
-          <Link href={'/#login'}>Login</Link>
-      </nav>
-    </header>
+   <Header />
+   <Hero />
+
     </>
   )
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between ">
+    <Link className="text-primary font-semibold text-2xl" href="">PIZZA GHOR</Link>
+    <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+    <Link href={'/'}>Home</Link>
+        <Link href={'/menu'}>Menu</Link>
+        <Link href={'/#about'}>About</Link>
+        <Link href={'/#contact'}>Contact</Link>
+        <Link className="bg-primary rounded-full text-white px-6 py-2" href={'/#login'}>Login</Link>
+    </nav>
+  </header>
+  )
+}
+
+export default Header
