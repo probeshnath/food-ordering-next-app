26c93c55f88af8e41f7046425806d0dd337f6fcd
probeshnath <probeshn@gmail.com>
2023-12-14 02:13:35 +0600

    register page and user register function added

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,34 @@
-import Header from "@/components/layout/Header"
 import Hero from "@/components/layout/Hero"
 import HomeMenu from "@/components/layout/HomeMenu"
+import SectionHeaders from "@/components/layout/SectionHeaders"
 
 
 
 const Home = () => {
   return (
     <>
-   <Header />
-   <Hero />
-   <HomeMenu />
+      <Hero />
+      <HomeMenu />
 
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={'Our story'} mainHeader={'About Us'} />
+
+        <div className="text-gray-500 max-w-md mx-auto mt-8 flex flex-col gap-4">
+          <p className=" ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eum rem temporibus magni possimus illo velit sapiente minus alias nemo sunt placeat, soluta pariatur distinctio repudiandae dolorum, amet culpa qui?</p>
+          <p className=" ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eum rem temporibus magni possimus illo velit sapiente minus alias nemo sunt placeat, soluta pariatur distinctio repudiandae dolorum, amet culpa qui?</p>
+          <p className=" ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eum rem temporibus magni possimus illo velit sapiente minus alias nemo sunt placeat, soluta pariatur distinctio repudiandae dolorum, amet culpa qui?</p>
+        </div>
+
+      </section>
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Don/'t hesitate"} mainHeader={'Contact Us'} />
+
+        <div className=" mt-8">
+          <a className="text-4xl underline text-gray-500" href="tel:+8801764613220">+8801764613220</a>
+        </div>
+
+      </section>
+     
     </>
   )
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,13 +5,19 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between ">
     <Link className="text-primary font-semibold text-2xl" href="">PIZZA GHOR</Link>
+
     <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-    <Link href={'/'}>Home</Link>
+        <Link href={'/'}>Home</Link>
         <Link href={'/menu'}>Menu</Link>
         <Link href={'/#about'}>About</Link>
         <Link href={'/#contact'}>Contact</Link>
-        <Link className="bg-primary rounded-full text-white px-6 py-2" href={'/#login'}>Login</Link>
     </nav>
+
+    <nav>
+      <Link className="bg-primary rounded-full text-white px-6 mr-2 py-2" href={'/login'}>Login</Link>
+      <Link className="bg-primary rounded-full text-white px-6 py-2" href={'/register'}>Register</Link>
+    </nav>
+
   </header>
   )
 }
